466356e2012919cb2b4b1c3c9bef53e8895e2ed2
al-vorotov <al.vorotov@gmail.com>
2021-06-22 02:21:58 +0300

    less fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+	Route,
+	Switch,
+	Redirect,
+} from "react-router-dom";
+import Main from "./layout/Main/Main";
+import Calc1 from "./layout/Calc1/Calc";
+import Calc2 from "./layout/Calc2/Calc";
+import Calc3 from "./layout/Calc3/Calc";
+import Nav from "./layout/Nav/Nav";
+import Contact from "./layout/Contact/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='all-container'>
+			<Nav/>
+			<div className='all-wrapper'>
+				<Switch>
+					<Route exact path="/main">
+						<Main/>
+					</Route>
+					<Route exact path="/calc1">
+						<Calc1/>
+					</Route>
+					<Route exact path="/calc2">
+						<Calc2/>
+					</Route>
+					<Route exact path="/calc3">
+						<Calc3/>
+					</Route>
+					<Route exact path="/contact">
+						<Contact/>
+					</Route>
+					<Redirect from='/' to='main' />
+				</Switch>
+			</div>
+		</div>
+	);
 }
 
 export default App;
